280f3771697c55e103918cf6d64bf9cba69358fe
magalhas <magalhas@gmail.com>
2014-02-16 20:20:47 +0000

    first jasmine unit tests (basic data binding tests)

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,8 +10,23 @@ exports = module.exports = function (grunt) {
           'dist/backbone-react-component-min.js': ['lib/component.js']
         }
       }
+    },
+    jasmine: {
+      main: {
+        src: ['lib/**/*.js'],
+        options: {
+          specs: 'test/**/*.js',
+          vendor: [
+            'bower_components/underscore/underscore.js',
+            'bower_components/backbone/backbone.js',
+            'bower_components/react/react.js'
+          ]
+        }
+      }
     }
   });
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('test', ['jasmine']);
 };
---
+++ test/component.js
@@ -0,0 +1,77 @@
+describe('Component', function () {
+  'use strict';
+  var Component = Backbone.React.Component.extend({
+    render: function () {
+      return React.DOM.div({}, this.props.test);
+    }
+  });
+  var component, model1, model2, collection1, collection2;
+  beforeEach(function () {
+    model1 = new Backbone.Model({
+      test: 'A'
+    });
+    model2 = new Backbone.Model({
+      otherTest: 'B'
+    });
+    collection1 = new Backbone.Collection([model1, model2]);
+    collection2 = new Backbone.Collection([model2, model1]);
+  });
+  afterEach(function () {
+    if (component) component.remove();
+    component = void 0;
+    model1.stopListening();
+    model2.stopListening();
+    collection1.stopListening();
+    collection2.stopListening();
+  });
+  // Not passing due to phantomjs polyfill? Need to confirm this
+  xit('renders', function () {
+    component = new Component({
+      model: model1,
+      el: document.body
+    }).mount();
+    expect(component.el.childNodes.length).toEqual(1);
+    expect(component.el.childNodes[0].tagName).toEqual('DIV');
+  });
+  it('binds to a model', function () {
+    component = new Component({model: model1});
+    expect(component.props.test).toEqual('A');
+    model1.set('test', 'B');
+    expect(component.props.test).toEqual('B');
+  });
+  it('binds to multiple models', function () {
+    component = new Component({
+      model: {
+        model1: model1,
+        model2: model2
+      }
+    });
+    expect(component.props.model1.test).toEqual('A');
+    expect(component.props.model2.otherTest).toEqual('B');
+  });
+  it('binds to a collection', function () {
+    component = new Component({
+      collection: collection1
+    });
+    expect(component.props.collection[0].test).toEqual('A');
+    expect(component.props.collection[1].otherTest).toEqual('B');
+    collection1.add({test: 'C'});
+    expect(component.props.collection[2].test).toEqual('C');
+  });
+  it('binds to multiple collections', function () {
+    component = new Component({
+      collection: {
+        firstCollection: collection1,
+        secondCollection: collection2
+      }
+    });
+    expect(component.props.firstCollection[0].test).toEqual('A');
+    expect(component.props.firstCollection[1].otherTest).toEqual('B');
+    expect(component.props.secondCollection[0].otherTest).toEqual('B');
+    expect(component.props.secondCollection[1].test).toEqual('A');
+    collection1.add({test: 'C'});
+    expect(component.props.firstCollection[2].test).toEqual('C');
+    collection2.add({otherTest: 'C'});
+    expect(component.props.secondCollection[2].otherTest).toEqual('C');
+  });
+});
